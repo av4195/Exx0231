ca02040bd962ac9e3a6ad9256ed8f78e232af80f
av4195 <av4195@bs.amalnet.k12.il>
2025-11-16 15:54:14 +0200

    Book

--- app/src/main/java/com/example/exx0231/Book.java
+++ app/src/main/java/com/example/exx0231/Book.java
@@ -1,4 +1,43 @@
 package com.example.exx0231;
 
-public class Book {
+public class Book extends Product{
+    private int numPage;
+
+
+    public Book(String name, int price, String description, int numPage){
+        super(name, price, description);
+        this.numPage = numPage;
+    }
+
+    public int getNumPage(){
+        return this.numPage;
+    }
+
+    public void setNumPage(int numPage){
+        this.numPage = numPage;
+    }
+
+    public String getName(){
+        return super.getName();
+    }
+
+    public int getPrice(){
+        return super.getPrice();
+    }
+
+    public String getDescription(){
+        return super.getDescription();
+    }
+
+    public void setName(String name){
+        super.setName(name);
+    }
+
+    public void setPrice(int price){
+        super.setPrice(price);
+    }
+
+    public void setDescription(String description){
+        super.setDescription(description);
+    }
 }
